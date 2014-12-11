35ed38a7d6647219064ed6e21cadf82424c81a14
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2014-12-11 11:55:06 +0100

    update deployment with local uaa

---
+++ local/check_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+: ${PERISCOPE_SMTP_USERNAME:?"SMTP username is missing from env_props.sh"}
+: ${PERISCOPE_SMTP_PASSWORD:?"SMTP password is missing from env_props.sh"}
+: ${PERISCOPE_SMTP_HOST:?"SMTP host is missing from env_props.sh"}
+: ${PERISCOPE_STMP_PORT:?"SMTP port is missing from env_props.sh"}
+: ${PERISCOPE_SMTP_FROM:?"SMTP from is missing from env_props.sh"}
+
+: ${PERISCOPE_CLOUDBREAK_URL:?"Cloudbreak's address is missing from env_props.sh"}
+
+echo Starting Periscope with the following settings:
+
+for p in "${!PERISCOPE_@}"; do
+  echo $p=${!p}
+done
---
+++ local/start_periscope.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+echo "Starting Periscope with a Postgres DB"
+
+if [ ! -f env_props.sh ] ;then
+  cp env_props.sh.sample_local env_props.sh
+  cat <<EOF
+=================================================
+= Please fill missing variables in:env_props.sh =
+=================================================
+EOF
+  exit
+fi
+
+source env_props.sh
+source check_env.sh
+
+if [ $? -ne 0 ]; then
+  exit 1;
+fi
+
+check-docker-version() {
+  DOCKER_VER=$(docker version|sed -n "/Server version/ {s/.*:.//; s/\.//gp}")
+  if [ $DOCKER_VER -lt 111 ]; then
+    cat <<EOF
+
+=============================================
+= ERROR                                     =
+= You are using an old version of Docker =
+= Please upgrade it to at least 1.1.1       =
+=============================================
+EOF
+    exit -1
+  fi
+}
+
+check-docker-version
+
+: ${PERISCOPE_DB_HBM2DDL_STRATEGY:=create}
+: ${PERISCOPE_DB_TCP_PORT:=5432}
+: ${PERISCOPE_SMTP_FROM:=no-reply@sequenceiq.com}
+: ${PERISCOPE_SMTP_PORT:=587}
+: ${PERISCOPE_CLIENT_ID:=periscope}
+: ${PERISCOPE_CLIENT_SECRET:=periscopesecret}
+: ${PERISCOPE_SERVER_PORT:=8081}
+: ${PERISCOPE_HOSTNAME_RESOLUTION:=private}
+
+: ${UAA_DOCKER_IMAGE_TAG:=latest}
+: ${PERISCOPE_DOCKER_IMAGE_TAG:=latest}
+
+docker pull sequenceiq/uaa:$UAA_DOCKER_IMAGE_TAG
+docker pull sequenceiq/periscope:$PERISCOPE_DOCKER_IMAGE_TAG
+
+docker inspect uaadb &>/dev/null && docker rm -f uaadb
+
+timeout=10
+docker run -d --name="uaadb" postgres
+echo "Wait $timeout seconds for the UAA DB to start up"
+sleep $timeout
+
+docker inspect uaa &>/dev/null && docker rm -f uaa
+
+timeout=20
+docker run -d --name="uaa" --link uaadb:db sequenceiq/uaa:$UAA_DOCKER_IMAGE_TAG
+timeout=20
+echo "Wait $timeout seconds for the UAA to start up"
+sleep $timeout
+
+# Removes previous containers
+docker inspect peripostgresql &>/dev/null && docker rm -f peripostgresql
+
+# Start a postgres database docker container
+docker run -d --name="peripostgresql" -p $PERISCOPE_DB_TCP_PORT:5432 postgres
+
+timeout=10
+echo "Wait $timeout seconds for the POSTGRES DB to start up"
+sleep $timeout
+
+docker inspect periscope &>/dev/null && docker rm -f periscope
+
+DB_ADDR=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" peripostgresql)
+UAA_ADDR=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" uaa)
+
+# Start the Periscope application
+docker run -d --name="periscope" \
+-e "PERISCOPE_DB_HBM2DDL_STRATEGY=$PERISCOPE_DB_HBM2DDL_STRATEGY" \
+-e "PERISCOPE_DB_TCP_PORT=5432" \
+-e "PERISCOPE_DB_TCP_ADDR=$DB_ADDR" \
+-e "PERISCOPE_SMTP_HOST=$PERISCOPE_SMTP_HOST" \
+-e "PERISCOPE_SMTP_USERNAME=$PERISCOPE_SMTP_USERNAME" \
+-e "PERISCOPE_SMTP_PASSWORD=$PERISCOPE_SMTP_PASSWORD" \
+-e "PERISCOPE_SMTP_FROM=$PERISCOPE_SMTP_FROM" \
+-e "PERISCOPE_SMTP_PORT=$PERISCOPE_SMTP_PORT" \
+-e "PERISCOPE_CLOUDBREAK_URL=$PERISCOPE_CLOUDBREAK_URL" \
+-e "PERISCOPE_IDENTITY_SERVER_URL=http://$UAA_ADDR:8080" \
+-e "PERISCOPE_CLIENT_ID=$PERISCOPE_CLIENT_ID" \
+-e "PERISCOPE_CLIENT_SECRET=$PERISCOPE_CLIENT_SECRET" \
+-e "PERISCOPE_HOSTNAME_RESOLUTION=$PERISCOPE_HOSTNAME_RESOLUTION" \
+-p $PERISCOPE_SERVER_PORT:8080 \
+sequenceiq/periscope:$PERISCOPE_DOCKER_IMAGE_TAG
+
+PERISCOPE_ADDR=$(docker inspect -f "{{.NetworkSettings.IPAddress}}" periscope)
+
+cat <<EOF
+================================================================
+Periscope is running on: $PERISCOPE_ADDR:$PERISCOPE_SERVER_PORT
+================================================================
+EOF
+
+docker logs -f periscope

624b784fbe3aeed02b3a6eba1374a4fe63c0f667
lalyos <lalyos@yahoo.com>
2015-10-27 16:09:57 +0100

    use the same script as cloudbreak

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,27 @@
-install() {
-  curl -L https://github.com/progrium/dockerhub-tag/releases/download/v0.2.0/dockerhub-tag_0.2.0_Darwin_x86_64.tgz | tar -xz -C /usr/local/bin/
+install_deps() {
+  if ! dockerhub-tag --version &>/dev/null ;then
+    echo "---> installing dockerhub-tag binary to /usr/local/bin" 1>&2
+    curl -L https://github.com/progrium/dockerhub-tag/releases/download/v0.2.0/dockerhub-tag_0.2.0_Darwin_x86_64.tgz | tar -xz -C /usr/local/bin/
+  else
+    echo "---> dockerhub-tag already installed" 1>&2
+  fi
 }
 
 new_version() {
-
+  install_deps
   declare NEW_VERSION=${1:? version required}
 
-  declare DOCKER_REPO:${2:? required cloudbreak/periscope/uluwatu-bin/sultans-bin}
   sed -i "/^ENV VERSION/ s/VERSION .*/VERSION ${NEW_VERSION}/" Dockerfile
 
   git commit -m "Release ${NEW_VERSION}" Dockerfile
   git tag ${NEW_VERSION}
   git push origin master --tags
   
-  dockerhub-tag set sequenceiq/$DOCKER_REPO $NEW_VERSION $NEW_VERSION /
+  dockerhub-tag set sequenceiq/periscope $NEW_VERSION $NEW_VERSION /
 }
 
+main() {
+  new_version "$@"
+}
+
+[[ "$0" ==  "$BASH_SOURCE" ]] && main "$@"
